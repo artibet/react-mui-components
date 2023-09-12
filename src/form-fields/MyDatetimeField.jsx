9b9e86f505d808c4381e066285722f3d1e5247a7
Tsakiridis Sotiris <sotiris@serres.gr>
2023-09-12 13:43:58 +0300

    Fix MyDatetimeField

--- src/form-fields/MyDatetimeField.jsx
+++ src/form-fields/MyDatetimeField.jsx
@@ -48,6 +48,7 @@ export const MyDatetimeField = ({
                 helperText={errors[name]?.message}
                 variant="outlined"
                 fullWidth={true}
+                {...props}
               />}
           />
         </LocalizationProvider>
