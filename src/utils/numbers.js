23a6c03eb105f4b1a29445465a94d266eeb37534
Tsakiridis Sotiris <sotiris@serres.gr>
2023-09-22 10:14:50 +0300

    Create formatFloat into utils/numbers

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -1,5 +1,5 @@
 // ---------------------------------------------------------------------------------------
-// 
+// format integer
 // ---------------------------------------------------------------------------------------
 export const formatInteger = (number) => {
   if (!number) return 0
@@ -7,12 +7,23 @@ export const formatInteger = (number) => {
 }
 
 // ---------------------------------------------------------------------------------------
-// 
+// format currency
 // ---------------------------------------------------------------------------------------
 export const formatCurrency = (num) => {
   const formatter = new Intl.NumberFormat('el-GR', {
     style: 'currency',
     currency: 'EUR'
+  })
+  return formatter.format(num)
+}
+
+// ---------------------------------------------------------------------------------------
+// format float
+// ---------------------------------------------------------------------------------------
+export const formatFloat = (num, decimals = 2) => {
+  const formatter = new Intl.NumberFormat('el-GR', {
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals
   })
   return formatter.format(num)
 }
