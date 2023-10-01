ef7fa4b48ce417bfb850cae1bc7c551990ec94d5
Tsakiridis Sotiris <artibetgr@gmail.com>
2023-10-01 14:44:26 +0300

    Create YesNoModalForm

---
+++ src/Modals/YesNoModalForm.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { Button, Dialog, DialogContent, DialogTitle, DialogActions } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { MyAutocompleteField } from '../form-fields'
+
+const options = [
+  { 'id': true, label: 'ΝΑΙ' },
+  { 'id': false, label: 'ΟΧΙ' },
+]
+
+export const YesNoModalForm = ({
+  open,
+  title,
+  label,
+  value,
+  onSubmit,
+  onCancel,
+  required = true,
+  width = 500,
+  requiredMessage = 'Υποχρεωτικό πεδίο',
+  okLabel = 'ΚΑΤΑΧΩΡΗΣΗ',
+  cancelLabel = 'ΑΚΥΡΩΣΗ',
+}) => {
+
+  // ---------------------------------------------------------------------------------------
+  // Default value
+  // ---------------------------------------------------------------------------------------
+  const defaultValues = {
+    field: value || null,
+  }
+
+
+  // ---------------------------------------------------------------------------------------
+  // Validation schema
+  // ---------------------------------------------------------------------------------------
+  const schema = yup.object({
+    field: required
+      ? yup
+        .boolean()
+        .required(requiredMessage)
+      : yup
+        .boolean()
+        .nullable()
+  })
+
+  // ---------------------------------------------------------------------------------------
+  // State and hooks
+  // ---------------------------------------------------------------------------------------
+  const form = useForm({ defaultValues, resolver: yupResolver(schema) })
+
+
+  // ---------------------------------------------------------------------------------------
+  // Reset textfield value
+  // ---------------------------------------------------------------------------------------
+  React.useEffect(() => {
+    if (open) {
+      form.clearErrors()
+      form.setValue('field', value, {
+        shouldValidate: false,
+        shouldDirty: false,
+        shouldTouch: false
+      })
+    }
+  }, [open, value])
+
+  // ---------------------------------------------------------------------------------------
+  // JSX
+  // ---------------------------------------------------------------------------------------
+  return (
+    <Dialog open={open} onClose={() => { }} >
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <MyAutocompleteField
+          form={form}
+          name='field'
+          label={label}
+          options={options}
+          valueKey='id'
+          labelKey='label'
+          required={required}
+          autofocus
+          sx={{ width: width, marginTop: 2, }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button color='success' onClick={form.handleSubmit(data => onSubmit(data.field))}>{okLabel}</Button>
+        <Button color='error' onClick={() => onCancel()}>{cancelLabel}</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
