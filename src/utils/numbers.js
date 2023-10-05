37c2bf6fd566bb5e762146045c4327bd6f16879c
Tsakiridis Sotiris <sotiris@serres.gr>
2023-10-05 11:23:07 +0300

    Fix number formating into utils

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -1,3 +1,5 @@
+import { isEmpty } from "./values";
+
 // ---------------------------------------------------------------------------------------
 // format integer
 // ---------------------------------------------------------------------------------------
@@ -10,6 +12,7 @@ export const formatInteger = (number) => {
 // format currency
 // ---------------------------------------------------------------------------------------
 export const formatCurrency = (num) => {
+  if (isEmpty(num)) return ''
   const formatter = new Intl.NumberFormat('el-GR', {
     style: 'currency',
     currency: 'EUR'
@@ -21,6 +24,7 @@ export const formatCurrency = (num) => {
 // format float
 // ---------------------------------------------------------------------------------------
 export const formatFloat = (num, decimals = 2) => {
+  if (isEmpty(num)) return
   const formatter = new Intl.NumberFormat('el-GR', {
     minimumFractionDigits: decimals,
     maximumFractionDigits: decimals
