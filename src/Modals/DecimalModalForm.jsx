519735481951b6391f8961f6a413381ae5091945
Tsakiridis Sotiris <sotiris@serres.gr>
2024-01-30 08:38:42 +0200

    Create DecimalModalForm

---
+++ src/Modals/DecimalModalForm.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Button, Dialog, DialogContent, DialogTitle, DialogActions } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { MyCurrencyField } from '../form-fields'
+import { notEmpty } from '../utils'
+
+
+export const DecimalModalForm = ({
+  open,
+  title,
+  label,
+  value,
+  onSubmit,
+  onCancel,
+  width = 500,
+  required = true,
+  requiredMessage = 'Υποχρεωτικό πεδίο',
+  okLabel = 'ΚΑΤΑΧΩΡΗΣΗ',
+  cancelLabel = 'ΑΚΥΡΩΣΗ'
+}) => {
+
+  // ---------------------------------------------------------------------------------------
+  // Default value
+  // ---------------------------------------------------------------------------------------
+  const defaultValues = {
+    field: notEmpty(value) ? value : null,
+  }
+
+  // ---------------------------------------------------------------------------------------
+  // Validation schema
+  // ---------------------------------------------------------------------------------------
+  const schema = yup.object({
+    field: required
+      ? yup
+        .string()
+        .required(requiredMessage)
+      : yup
+        .string()
+  })
+
+  // ---------------------------------------------------------------------------------------
+  // State and hooks
+  // ---------------------------------------------------------------------------------------
+  const form = useForm({ defaultValues, resolver: yupResolver(schema) })
+
+  // ---------------------------------------------------------------
+  // submit on key down
+  // ---------------------------------------------------------------
+  const handleKeyDown = e => {
+    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+      e.preventDefault()
+      form.handleSubmit(data => onSubmit(data.field))()
+    }
+  }
+
+  // ---------------------------------------------------------------------------------------
+  // Reset text value
+  // ---------------------------------------------------------------------------------------
+  React.useEffect(() => {
+    if (open) {
+      form.clearErrors()
+      form.setValue('field', notEmpty(value) ? value : null, {
+        shouldValidate: false,
+        shouldDirty: false,
+        shouldTouch: false
+      })
+    }
+  }, [open, value])
+
+  // ---------------------------------------------------------------------------------------
+  // JSX
+  // ---------------------------------------------------------------------------------------
+  return (
+    <Dialog open={open} onClose={() => { }} onKeyDown={handleKeyDown} disableRestoreFocus >
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <MyCurrencyField
+          form={form}
+          name='field'
+          label={label}
+          required={required}
+          autofocus={true}
+          sx={{ width: width, marginTop: 2, }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button color='success' onClick={form.handleSubmit(data => onSubmit(data.field))}>{okLabel}</Button>
+        <Button color='error' onClick={() => onCancel()}>{cancelLabel}</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
