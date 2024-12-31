d9a4fcbcbfbb09f5d3b46cea5f9d252a391eba8f
Tsakiridis Sotiris <artibetgr@gmail.com>
2025-01-01 00:19:34 +0200

    Fix x-dates incobatibility issue with date-fns

--- src/form-fields/MyDateField.jsx
+++ src/form-fields/MyDateField.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { TextField } from '@mui/material'
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DatePicker } from '@mui/x-date-pickers'
 import el from 'date-fns/locale/el'
