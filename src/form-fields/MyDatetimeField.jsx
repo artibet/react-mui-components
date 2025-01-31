f30a282d11a44c5e5bc9fd2e27b00912969781eb
Tsakiridis Sotiris <sotiris@serres.gr>
2025-01-31 19:46:00 +0200

    Work on new DateTimePicker

--- src/form-fields/MyDatetimeField.jsx
+++ src/form-fields/MyDatetimeField.jsx
@@ -5,59 +5,100 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DateTimePicker } from '@mui/x-date-pickers'
 import el from 'date-fns/locale/el'
 import { Controller } from 'react-hook-form'
+import { toDate } from 'date-fns'
 
 export const MyDatetimeField = ({
-  form,
-  name,
-  label,
-  required = false,
-  disabled = false,
-  autofocus = false,
-  showErrors = true,
-  ...props
+	form,
+	name,
+	label,
+	required = false,
+	disabled = false,
+	autofocus = false,
+	showErrors = true,
+	ampm = false,	// if false show 24 hours
+	format = "dd/MM/yyyy HH:mm",
+	...props
 }) => {
 
-  // ---------------------------------------------------------------------------------------
-  // Destructure form fiels
-  // ---------------------------------------------------------------------------------------
-  const { control, formState } = form
-  const { errors } = formState
+	// ---------------------------------------------------------------------------------------
+	// Destructure form fiels
+	// ---------------------------------------------------------------------------------------
+	const { control, formState } = form
+	const { errors } = formState
 
-  // ---------------------------------------------------------------------------------------
-  // JSX
-  // ---------------------------------------------------------------------------------------
-  return (
-    <Controller
-      name={name}
-      control={control}
-      rules={{
-        required: required,
-        disabled: disabled
-      }}
-      render={({ field: { onChange, onBlur, value } }) => (
-        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={el}>
-          <DateTimePicker
-            label={label}
-            inputFormat="dd/MM/yyyy HH:mm"
-            value={value}
-            onChange={onChange}
-            disabled={disabled}
-            slots={{
-              textField: params =>
-                <TextField {...params}
-                  required={required}
-                  error={showErrors && Boolean(errors[name]?.message)}
-                  helperText={showErrors && errors[name]?.message}
-                  variant="outlined"
-                  fullWidth={true}
-                  {...props}
-                />
-            }}
-          />
-        </LocalizationProvider>
-      )}
+	// ---------------------------------------------------------------------------------------
+	// JSX
+	// ---------------------------------------------------------------------------------------
+	return (
+		<Controller
+			name={name}
+			control={control}
+			rules={{
+				required: required,
+				disabled: disabled
+			}}
+			render={({ field }) => (
+				<LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={el}>
+					<DateTimePicker
+						{...field}
+						label={label}
+						// format={format}
+						ampm={ampm}
+						slots={{
+							textField: params =>
+								<TextField {...params}
+									required={required}
+									error={showErrors && Boolean(errors[name]?.message)}
+									helperText={showErrors && errors[name]?.message}
+									variant="outlined"
+									fullWidth={true}
+									{...props}
+								/>,
+						}}
+					/>
+				</LocalizationProvider>
+			)}
+		/>
 
-    />
-  )
+
+
+		// <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={el}>
+		// 	<Controller
+		// 		name={name}
+		// 		control={control}
+		// 		rules={{
+		// 			required: required,
+		// 			disabled: disabled
+		// 		}}
+		// 		render={({ field }) => (
+		// 			<DateTimePicker
+		// 				label={label}
+		// 				format={format}
+		// 				value={field.value}
+		// 				views={["year", "month", "day", "hours", "minutes"]}
+		// 				// viewRenderers={{
+		// 				// 	hours: renderDigitalClockTimeView,
+		// 				// 	minutes: renderDigitalClockTimeView
+		// 				// }}
+		// 				ampm={ampm}
+		// 				onChange={field.onChange}
+		// 				disabled={disabled}
+		// 				slots={{
+		// 					textField: params =>
+		// 						<TextField {...params}
+		// 							required={required}
+		// 							error={showErrors && Boolean(errors[name]?.message)}
+		// 							helperText={showErrors && errors[name]?.message}
+		// 							variant="outlined"
+		// 							fullWidth={true}
+		// 							{...props}
+		// 						/>,
+		// 				}}
+		// 			/>
+		// 		)}
+		// 	/>
+		// </LocalizationProvider>
+	)
 }
+
 
