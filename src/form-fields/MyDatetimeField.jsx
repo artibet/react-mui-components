f1950dbaa4580638bff3fb988f9cb72603abe8c0
Tsakiridis Sotiris <sotiris@serres.gr>
2025-02-03 08:34:57 +0200

    Update MyDateTimeField

--- src/form-fields/MyDatetimeField.jsx
+++ src/form-fields/MyDatetimeField.jsx
@@ -15,6 +15,10 @@ export const MyDatetimeField = ({
   autofocus = false,
   showErrors = true,
   ampm = false,	// if false show 24 hours
+  singleTimeColumn = true,
+  hoursStep = 1,
+  minutesStep = 15,
+  secondsStep = 30,
   ...props
 }) => {
 
@@ -23,6 +27,33 @@ export const MyDatetimeField = ({
   // ---------------------------------------------------------------------------------------
   const { control, formState } = form
   const { errors } = formState
+
+  // ---------------------------------------------------------------------------------------
+  // view based on singleTimeColumn prop
+  // ---------------------------------------------------------------------------------------
+  const views = () => {
+    if (singleTimeColumn) {
+      return ["year", "month", "day", "hours"]
+    }
+    else {
+      return ["year", "month", "day", "hours", "minutes"]
+    }
+
+  }
+
+  // ---------------------------------------------------------------------------------------
+  // viewRenderers base on singleTimeColumn prop
+  // ---------------------------------------------------------------------------------------
+  const viewRenderers = () => {
+    if (singleTimeColumn) {
+      return {
+        hours: renderDigitalClockTimeView,
+      }
+    }
+    else {
+      return null
+    }
+  }
 
   // ---------------------------------------------------------------------------------------
   // JSX
@@ -52,9 +83,15 @@ export const MyDatetimeField = ({
             value={field.value ? toDate(field.value) : null}
             format='dd/MM/yyyy HH:mm'
             inputFormat
-            views={["year", "month", "day", "hours", "minutes"]}
+            views={views()}
             ampm={ampm}
             onChange={newValue => field.onChange(newValue)}
+            viewRenderers={viewRenderers()}
+            timeSteps={{
+              hours: hoursStep,
+              minutes: minutesStep,
+              seconds: secondsStep,
+            }}
             slotProps={{
               textField: {
                 ...props,
@@ -63,7 +100,8 @@ export const MyDatetimeField = ({
                 helperText: showErrors && errors[name]?.message,
                 variant: 'outlined',
                 fullWidth: true,
-              }
+              },
+
             }}
           />
         )}
