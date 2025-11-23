720724d3272a164d9f1bd9e103ccab0a57001f2e
Tsakiridis Sotiris <artibetgr@gmail.com>
2025-11-23 10:27:10 +0200

    Update MyTimeField and create TimeModalForm

---
+++ src/Modals/TimeModalForm.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import { Button, Dialog, DialogContent, DialogTitle, DialogActions } from '@mui/material'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+import { MyTimeField } from '../form-fields'
+
+export const TimeModalForm = ({
+  open,
+  title,
+  label,
+  value,
+  onSubmit,
+  onCancel,
+  size = 'sm',  // sm, md, lg xl
+  required = true,
+  requiredMessage = 'Υποχρεωτικό πεδίο',
+  okLabel = 'ΚΑΤΑΧΩΡΗΣΗ',
+  cancelLabel = 'ΑΚΥΡΩΣΗ'
+}) => {
+
+  // ---------------------------------------------------------------------------------------
+  // Default value
+  // ---------------------------------------------------------------------------------------
+  const defaultValues = {
+    datetime: value,
+  }
+
+  // ---------------------------------------------------------------------------------------
+  // Validation schema
+  // ---------------------------------------------------------------------------------------
+  const schema = yup.object({
+    datetime: required
+      ? yup
+        .date()
+        .typeError('Μη έγκυρη μορφή ώρας')
+        .required(requiredMessage)
+      : yup
+        .date()
+        .typeError('Μη έγκυρη μορφή ώρας')
+  })
+
+  // ---------------------------------------------------------------------------------------
+  // State and hooks
+  // ---------------------------------------------------------------------------------------
+  const form = useForm({ defaultValues, resolver: yupResolver(schema) })
+
+  // ---------------------------------------------------------------
+  // submit on key down
+  // ---------------------------------------------------------------
+  const handleKeyDown = e => {
+    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+      e.preventDefault()
+      form.handleSubmit(data => onSubmit(data.datetime))()
+    }
+  }
+
+  // ---------------------------------------------------------------------------------------
+  // Reset date value
+  // ---------------------------------------------------------------------------------------
+  React.useEffect(() => {
+    if (open) {
+      form.clearErrors()
+      form.setValue('datetime', value, {
+        shouldValidate: false,
+        shouldDirty: false,
+        shouldTouch: false
+      })
+    }
+  }, [open, value])
+
+  // ---------------------------------------------------------------------------------------
+  // JSX
+  // ---------------------------------------------------------------------------------------
+  return (
+    <Dialog fullWidth maxWidth={size} open={open} onClose={() => { }} onKeyDown={handleKeyDown} disableRestoreFocus >
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <MyTimeField
+          form={form}
+          name='datetime'
+          label={label}
+          required={required}
+          autofocus={true}
+          sx={{ marginTop: 2, }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button color='success' onClick={form.handleSubmit(data => onSubmit(data.datetime))}>{okLabel}</Button>
+        <Button color='error' onClick={() => onCancel()}>{cancelLabel}</Button>
+      </DialogActions>
+    </Dialog >
+  )
+}
--- src/form-fields/MyTimefield.jsx
+++ src/form-fields/MyTimefield.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { TextField } from '@mui/material'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { TimePicker } from '@mui/x-date-pickers'
+import { TimePicker, renderDigitalClockTimeView } from '@mui/x-date-pickers'
 import el from 'date-fns/locale/el'
 import { Controller } from 'react-hook-form'
+import { toDate } from 'date-fns'
 
 export const MyTimeField = ({
   form,
   name,
   label,
+  ampm = false,	// if false show 24 hours
+  hoursStep = 1,
+  minutesStep = 15,
+  secondsStep = 30,
+  singleTimeColumn = true,
   required = false,
   disabled = false,
   autofocus = false,
@@ -24,40 +29,83 @@ export const MyTimeField = ({
   const { errors } = formState
 
   // ---------------------------------------------------------------------------------------
+  // view based on singleTimeColumn prop
+  // ---------------------------------------------------------------------------------------
+  const views = () => {
+    if (singleTimeColumn) {
+      return ["hours"]
+    }
+    else {
+      return ["hours", "minutes"]
+    }
+
+  }
+  // ---------------------------------------------------------------------------------------
+  // viewRenderers base on singleTimeColumn prop
+  // ---------------------------------------------------------------------------------------
+  const viewRenderers = () => {
+    if (singleTimeColumn) {
+      return {
+        hours: renderDigitalClockTimeView,
+      }
+    }
+    else {
+      return null
+    }
+  }
+
+  // ---------------------------------------------------------------------------------------
   // JSX
   // ---------------------------------------------------------------------------------------
   return (
-    <Controller
-      name={name}
-      control={control}
-      rules={{
-        required: required,
-        disabled: disabled
+    <LocalizationProvider
+      dateAdapter={AdapterDateFns}
+      adapterLocale={el}
+      localeText={{
+        fieldDayPlaceholder: _ => 'ΗΗ',
+        fieldMonthPlaceholder: _ => 'ΜΜ',
+        fieldYearPlaceholder: _ => 'EEEE',
+        fieldHoursPlaceholder: _ => 'ΩΩ',
+        fieldMinutesPlaceholder: _ => 'ΛΛ',
       }}
-      render={({ field: { onChange, onBlur, value } }) => (
-        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={el}>
+    >
+      <Controller
+        name={name}
+        control={control}
+        rules={{
+          required: required,
+          disabled: disabled
+        }}
+        render={({ field }) => (
           <TimePicker
             label={label}
-            inputFormat="HH:mm"
-            value={value}
-            onChange={onChange}
-            disabled={disabled}
-            slots={{
-              textField: params =>
-                <TextField {...params}
-                  required={required}
-                  error={showErrors && Boolean(errors[name]?.message)}
-                  helperText={showErrors && errors[name]?.message}
-                  variant="outlined"
-                  fullWidth={true}
-                  {...props}
-                />
+            value={field.value ? toDate(field.value) : null}
+            format='HH:mm'
+            inputFormat
+            views={views()}
+            ampm={ampm}
+            onChange={newValue => field.onChange(newValue)}
+            viewRenderers={viewRenderers()}
+            timeSteps={{
+              hours: hoursStep,
+              minutes: minutesStep,
+              seconds: secondsStep,
+            }}
+            slotProps={{
+              textField: {
+                ...props,
+                required: required,
+                error: showErrors && Boolean(errors[name]?.message),
+                helperText: showErrors && errors[name]?.message,
+                variant: 'outlined',
+                fullWidth: true,
+              },
+
             }}
           />
-        </LocalizationProvider>
-      )}
-
-    />
+        )}
+      />
+    </LocalizationProvider >
   )
 }
 
