f53517615ea8668310c7f79f6abdd5764a3b5d4c
Tsakiridis Sotiris <sotiris@serres.gr>
2026-02-10 11:56:08 +0200

    Add click handler to table create button

--- src/tables/ServerSideTable/CreateButton.jsx
+++ src/tables/ServerSideTable/CreateButton.jsx
@@ -13,11 +13,23 @@ const CreateButton = () => {
 
   if (!props.enableCreateRow) return null
 
+  // ---------------------------------------------------------------------------------------
+  // click handler
+  // ---------------------------------------------------------------------------------------
+  const handleClick = e => {
+    e.currentTarget.blur()
+    props.onCreateRow(e)
+  }
+
+  // ---------------------------------------------------------------------------------------
+  // JSX
+  // ---------------------------------------------------------------------------------------
   return (
     <Tooltip title={props.createButtonTooltip} arrow={true} placement='top'>
       <Button
         variant={props.createButtonVariant}
-        onClick={() => props.onCreateRow()}>
+        onClick={handleClick}
+      >
         <MdOutlineAdd size={24} />
       </Button>
     </Tooltip>
