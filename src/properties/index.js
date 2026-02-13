be1b114cf728f35d01b351b4606900eeb8b17a34
Tsakiridis Sotiris <sotiris@serres.gr>
2026-02-13 07:50:33 +0200

    Add DateProperty to exports

--- src/properties/index.js
+++ src/properties/index.js
@@ -1,6 +1,7 @@
 export * from './StringProperty'
 export * from './EmailProperty'
 export * from './DateTimeProperty'
+export * from './DateProperty'
 export * from './TextProperty'
 export * from './AutocompleteProperty'
 export * from './ToggleProperty'
